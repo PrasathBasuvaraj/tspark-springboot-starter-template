e3a7a6214139c3bb44306504109fb9b93a06bfb7
Prasath Basuvaraj <prasath.buddie@gmail.com>
2025-02-16 03:23:46 -0500

    OpenAPI Annotations

--- src/main/java/org/techspark/GreetingController.java
+++ src/main/java/org/techspark/GreetingController.java
@@ -1,9 +1,14 @@
 package org.techspark;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/greetings")
+@Tag(name = "Greeting",
+        description = "A Greeting message from TechSpark")
 public class GreetingController {
     @GetMapping("/")
     public String greeting() {
